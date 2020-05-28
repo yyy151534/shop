278912ac970a74a4f3f06ebfc1b8a41755dbdaa5
hfyu <yuhf@weherepost.com>
2020-05-28 10:26:36 +0800

    swagger

--- gateway/src/main/java/com/xuan/gateway/GatewayApp.java
+++ gateway/src/main/java/com/xuan/gateway/GatewayApp.java
@@ -2,9 +2,11 @@ package com.xuan.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan("com.xuan")
+@EnableFeignClients
 @SpringCloudApplication
 public class GatewayApp {
     public static void main(String[] args) {
